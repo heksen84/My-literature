b478583fb3949201156bb9411fc9ca0106fdb647
Илья <>
2017-05-05 22:23:29 +0600

    ok

--- pages/js/my_works.js
+++ pages/js/my_works.js
@@ -6,14 +6,27 @@
 $(document).ready(function() 
 {		
 	sweetAlertInitialize();	
+	
+    // ---------------------------
+    // обновить таблицу
+    // ---------------------------
+    function UpdateMyWorksTable()
+    {	
+		var i = 1;
+        var num_strings = $("#my_works_tabletr").length;
+        $("#my_works_table tbody tr td:first-child").each(function() {
+            if (i != num_strings) $(this).html(i);
+            i++;
+        });       
+	}
 		
 	$.ajax
 	({
 		url: "..//server.php",
 		data: 
-		{
-			"func": "SRV_GetWriterWorks" 
-		},
+			{
+				"func": "SRV_GetWriterWorks" 
+			},
 	}).done(function( data )				
 	{																
 		var obj = jQuery.parseJSON(data);					
@@ -21,20 +34,24 @@ $(document).ready(function()
 		{
 			case "error": error(obj.string); break;
 			case "warning": warning(obj.string); break;
-			case "success": {				
+			case "success": 
+			{				
 				$.each(obj.string, function(i, item) 
 				{
 					$("#tbody").append("<tr data-id='"+item.id+"'><td>"+(i+1)+"</td><td class='table_item'>"+item.title+"</td><td class='delete_record' title='удалить запись'>X</td></tr>");					
 				});
-															
+				
+				/* показать запись в редакторе */
 				$(".table_item").click(function() 
 				{
 					localStorage.setItem("read_data_id", $(this).parent().data("id"));
 					$(location).attr('href', "writer.php");
 				});
-
+				
+				/* удалить запись */
 				$(".delete_record").click(function() 
 				{
+					var element = $(this);
 					swal
 					({	
 						title: "Удалить работу?",
@@ -51,12 +68,13 @@ $(document).ready(function()
 					function(isConfirm) 
 					{
 						if (isConfirm) 
-						{	
+						{
+							element.parent().remove();
+							UpdateMyWorksTable();
 						}
 					});					
 				});
 			}
 		}		
 	});
-
 });
