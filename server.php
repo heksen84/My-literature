594ceb91e807638fd86b069a1a94f40ae572d2aa
Илья <>
2017-05-08 20:18:06 +0600

    ok

--- server.php
+++ server.php
@@ -108,13 +108,13 @@ switch(strtoupper($_SERVER["REQUEST_METHOD"])) {
 					break;    		
 				}				
 
-				// --- закладка ---
+				// -- закладка --
 				case "SRV_SetBookMark": {
 					$bookmark->set();
 					break;    		
 				}
 				
-				// 
+				// -- режим отображения записи --
 				case "SRV_SetRecordShowMode": {
 					$rec->setShowMode();
 					break;    		
