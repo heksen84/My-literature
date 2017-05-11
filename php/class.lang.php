29db78d42ffec4354155c7737a86730f522f7741
Илья <>
2017-05-11 13:50:39 +0600

    ok

--- php/class.lang.php
+++ php/class.lang.php
@@ -12,22 +12,8 @@ class Lang
 		}
 	}
 						
-	/* --- регистрация --- */
-	function register()
-	{		
-		$db	= DataBase::getDB();		
-				
-		if (!isset($_GET['type']) || !isset($_GET['name']) || !isset($_GET['surname']) || !isset($_GET['email']) || !isset($_GET['password'])) 
-			msg::error("нет данных");
-				
-		$type 	  = $_GET['type'];
-		$name 	  = $_GET['name'];
-		$surname  = $_GET['surname'];
-	        $email    = $_GET['email'];
-        	$password = $_GET['password'];		
-				
-		msg::success($name);
-	}
- 
+	function set($lang)
+	{			
+	} 
 }
 ?>
