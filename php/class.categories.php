66b65c8149294bd9eaf1aab7929326187f5e1688
Илья <>
2017-05-14 22:56:30 +0600

    ok

--- php/class.categories.php
+++ php/class.categories.php
@@ -22,10 +22,11 @@ class Categories
 	function getFromId()
 	{
 		$db	= DataBase::getDB();		
+		
 		$category_id = (int)$_GET["category_id"];
 		$start 	     = (int)$_GET["start"];
-		$records = $db->select(	
-		"SELECT records.id, records.title, users.surname, users.name, users.surname FROM `records` 
+		
+		$records = $db->select("SELECT records.id, records.title, users.surname, users.name, users.surname FROM `records` 
 		INNER JOIN `users` ON records.user_id=users.id AND records.type_literature='".$category_id."' AND records.access_mode!=1 LIMIT ".$start.",50");		
 		msg::success($records);
 	}
