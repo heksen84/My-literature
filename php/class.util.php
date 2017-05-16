a6b45324013dd21c13e0ba498bcd44cd64017317
Илья <>
2017-05-16 17:31:19 +0600

    ok

--- php/class.util.php
+++ php/class.util.php
@@ -10,20 +10,27 @@ class util
     	$month = date("m");
 	$name  = uniqid();
 
-	$fp = fopen("html/ru/".$year."/".$name.'.html', 'w+');	
-	fwrite($fp, "<!DOCTYPE html>");
-	fwrite($fp, "<html lang='ru'>");
-	fwrite($fp, "<head>");
-	fwrite($fp, "<meta charset='utf-8'>");
-	fwrite($fp, "<meta name='viewport' content='width=device-width'/>");
-	fwrite($fp, "<meta name='description' content='".$desc."'/>");
-	fwrite($fp, "<meta name='keywords' content='литература'/>");
-	fwrite($fp, "<meta name='robots' content='index, follow'/>");
-	fwrite($fp, "<title>".$title."</title>");
-	fwrite($fp, "</head>");
-	fwrite($fp, "<script>window.location='http://".$_SERVER['SERVER_NAME']."/?rec_id=".$id."'</script>");
-	fwrite($fp, "<body>https://www.my-literature.com</body>");
-	fclose($fp);
+	$fp = fopen("index/ru/".$year."/".$name.'.html', 'w+');	
+	if ($fp)
+	{
+		fwrite($fp, "<!DOCTYPE html>");
+		fwrite($fp, "<html lang='ru'>");
+		fwrite($fp, "<head>");
+		fwrite($fp, "<meta charset='utf-8'>");
+		fwrite($fp, "<meta name='viewport' content='width=device-width'/>");
+		fwrite($fp, "<meta name='description' content='".$desc."'/>");
+		fwrite($fp, "<meta name='keywords' content='литература'/>");
+		fwrite($fp, "<meta name='robots' content='index, follow'/>");
+		fwrite($fp, "<title>".$title."</title>");
+		fwrite($fp, "</head>");
+		fwrite($fp, "<script>window.location='http://".$_SERVER['SERVER_NAME']."/?rec_id=".$id."'</script>");
+		fwrite($fp, "<body>https://www.my-literature.com</body>");
+		fclose($fp);
+
+		// СДЕЛАТЬ ПРОВЕРКУ НА ОШИБКИ
+		$db = DataBase::getDB();
+		$result = $db->query("INSERT INTO `index-pages` VALUES (NULL, '".$id."', '".$name."')");		
+	}
  }
 }
 ?>
