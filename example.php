0e58509f9ff838e4e5e8e2d4477e76dd34a1d36c
Илья <>
2017-05-17 00:01:19 +0600

    ok

--- example.php
+++ example.php
@@ -1,14 +1,14 @@
 <?php
 
 $sitemap = simplexml_load_file("sitemap.xml");
-foreach ($sitemap->url as $url_list) {
+
+foreach ($sitemap->url as $url_list) 
+{
     $url = $url_list->loc;
-    if (strpos($url, "546"))
-    {
+    if (strpos($url, "545")) {
       unset($url_list[0]); 
     }
 }
-
 
 $dom = new DOMDocument('1.0');
 $dom->preserveWhiteSpace = false;
