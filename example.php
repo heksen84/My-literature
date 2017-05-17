e5be2ea149fe63ba0b73639d387fd6f654df28ca
Илья <>
2017-05-17 13:37:32 +0600

    ok

--- example.php
+++ example.php
@@ -5,13 +5,14 @@ $newxml = new SimpleXMLElement("<urlset xmlns='http://www.sitemaps.org/schemas/s
 
 foreach ($sitemap->url as $url_list) 
 {
-    if (!strstr($url_list->loc, "52")) 
-//    if (preg_match("/1/", $url_list->loc)==1)
+    if (!strpos($url_list->loc, "123"))
     {
       $newurl = $newxml->addChild("url");
       $newurl->addChild("loc",        $url_list->loc);
       $newurl->addChild("changefreq", $url_list->changefreq);
       $newurl->addChild("priority",   $url_list->priority);
+      echo "<br>ok";
+      //break;
     }
 }
 
