17df48c9d42772c6d9eb8a588ca134c473d97ced
Илья <>
2017-05-17 22:50:53 +0600

    ok

--- php/class.record.php
+++ php/class.record.php
@@ -44,7 +44,7 @@ class Record
 
 		$text_length = strlen($text);
 
-		if ( $text_length > 30) {			
+		if ( $text_length > 15) {			
 			if (!$this->compareText($text)) msg::error("Похоже, что такой текст уже присутсвует в базе!");			
 			$record_id = $db->query("INSERT INTO `records` VALUES (NULL,'".$_SESSION["user_id"]."','".$title."','".$desc."','".$type."','0','".$text."','".$mode."','".$price."',NOW())");		
 			if ($mode != 1 && $text_length > 100) util::GeneratePage($title, $desc, $record_id);				
