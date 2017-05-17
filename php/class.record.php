6ab0dbac5025ddbbf34a36bd8dc1bc9872ca2b9b
Илья <>
2017-05-17 13:49:46 +0600

    ok

--- php/class.record.php
+++ php/class.record.php
@@ -70,7 +70,27 @@ class Record
 		if (file_exists($file)){
 			$db->query("DELETE FROM `index-pages` WHERE record_id='".$record_id."'");
 			unlink($file);			
-		}				
+		}
+
+		$sitemap = simplexml_load_file("sitemap.xml");
+		$newxml = new SimpleXMLElement("<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'></urlset>");
+		
+		foreach ($sitemap->url as $url_list) 
+		{
+		    if (!strpos($url_list->loc, (string)$record_id))
+		    {
+		      $newurl = $newxml->addChild("url");
+		      $newurl->addChild("loc",        $url_list->loc);
+		      $newurl->addChild("changefreq", $url_list->changefreq);
+		      $newurl->addChild("priority",   $url_list->priority);
+		    }
+		}
+
+		$dom = new DOMDocument('1.0');
+		$dom->preserveWhiteSpace = false;
+		$dom->formatOutput = true;
+		$dom->loadXML($newxml->asXML());
+		$dom->save('sitemap.xml');				
 		
         msg::success($item);
 	}
