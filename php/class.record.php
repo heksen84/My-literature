98e35cbbdfa01388b37513c4af25f9ee8e7cd2c5
Илья <>
2017-05-17 22:05:54 +0600

    ok

--- php/class.record.php
+++ php/class.record.php
@@ -20,7 +20,7 @@ class Record
 		foreach ($result as $value) {
 		    if (!empty($value["text"])){
 		       similar_text($value["text"], $string1, $percent); 		    
-		       if ( $percent > 30 ) 
+		       if ( $percent > 50 ) 
                        return false;
                     }
 		}
@@ -44,10 +44,10 @@ class Record
 
 		$text_length = strlen($text);
 
-		if ( $text_length > 20) {			
+		if ( $text_length > 30) {			
 			if (!$this->compareText($text)) msg::error("Похоже, что такой текст уже присутсвует в базе!");			
 			$record_id = $db->query("INSERT INTO `records` VALUES (NULL,'".$_SESSION["user_id"]."','".$title."','".$desc."','".$type."','0','".$text."','".$mode."','".$price."',NOW())");		
-			if ($mode != 1) util::GeneratePage($title, $desc, $record_id);				
+			if ($mode != 1 && $text_length > 100) util::GeneratePage($title, $desc, $record_id);				
 			msg::success("опубликовано!");			
 		}
 		else msg::warning("Слишком мало текста для публикации!");							
