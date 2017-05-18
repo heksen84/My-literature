d5494a3ea24c20630d0bff5a8b13483236b18c4c
Илья <>
2017-05-18 15:31:48 +0600

    ok

--- php/class.categories.php
+++ php/class.categories.php
@@ -2,9 +2,9 @@
 
 class Categories
 {	
-	private static $categories = null;	
+	const SHOW_MAX_RECORDS = 100;	
 
-	const show_max_records = 100;	
+	private static $categories = null;	
 	
 	public static function getCategories() {	
 		if (self::$categories == null){			
@@ -28,7 +28,7 @@ class Categories
 		$category_id = (int)$_GET["category_id"];
 		$start 	     = (int)$_GET["start"];		
 		$records = $db->select("SELECT records.id, records.title, users.surname, users.name, users.surname FROM `records` 
-		INNER JOIN `users` ON records.user_id=users.id AND records.type_literature='".$category_id."' AND records.access_mode!=1 LIMIT ".$start.",".self::show_max_records);
+		INNER JOIN `users` ON records.user_id=users.id AND records.type_literature='".$category_id."' AND records.access_mode!=1 LIMIT ".$start.",".self::SHOW_MAX_RECORDS);
 		msg::success($records);
 	}
 }
