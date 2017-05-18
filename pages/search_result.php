2d9bba3db3f28396c0a833be9a4e0a4c02c56af5
Илья <>
2017-05-18 16:10:21 +0600

    ok

--- pages/search_result.php
+++ pages/search_result.php
@@ -41,9 +41,9 @@
   <thead>
     <tr>      
       <th style="width:5%">№</th>      
-      <th style="width:70%">название</th>
-      <th style="width:18%;text-align:center">разместил</th>	  	  
-      <th style="width:20%;text-align:center">&#10084;</th>
+      <th style="width:65%">название</th>
+      <th style="width:20%;text-align:center">разместил</th>	  	  
+      <th style="width:10%;text-align:center">&#10084;</th>
     </tr>
   </thead>
   <tbody id="tbody"></tbody>
