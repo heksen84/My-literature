f70fa50be0cfb909862d37e8571d6b699cd919f1
Илья <>
2017-05-19 13:26:18 +0600

    ok

--- pages/posted.php
+++ pages/posted.php
@@ -37,7 +37,7 @@
   <thead>
     <tr>      
       <th style="width:5%">№</th>      
-      <th style="width:70%">название</th>
+      <th style="width:60%">название</th>
       <th style="width:5%;text-align:center">&#10084;</th>
       <th style="width:5%;text-align:center">статус</th>
       <th style="width:5%;text-align:center"></th>
