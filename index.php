6023f34fe1c355e4198464569d2665abec142a93
Илья <>
2017-05-25 23:31:25 +0600

    ok

--- index.php
+++ index.php
@@ -49,6 +49,17 @@
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/44740402" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
+<!-- Google analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-99922016-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <script src = "pages/js/lib/jquery-3.2.1.min.js"></script>
 <script src = "pages/js/lib/tether.min.js"></script>
 <script src = "pages/js/lib/bootstrap.min.js"></script>
