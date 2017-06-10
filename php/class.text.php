fc02697aff6ce52aa49b72a1d059f3085f3cf938
Илья <>
2017-06-10 23:30:13 +0600

    ok

--- php/class.text.php
+++ php/class.text.php
@@ -16,8 +16,8 @@ class Text
 	function read()
 	{
 		$db = DataBase::getDB();
-//		$result = $db->select("SELECT * FROM records WHERE id='".$_GET["record_id"]."' LIMIT 1");
-		$result = $db->select("SELECT id, SUBSTR(CAST(text as CHAR),1,".self::MAX_READ_SYMBOLS.") AS text FROM records WHERE id='".$_GET["record_id"]."' LIMIT 1");
+		$result = $db->select("SELECT * FROM records WHERE id='".$_GET["record_id"]."' LIMIT 1");
+//		$result = $db->select("SELECT id, SUBSTR(CAST(text as CHAR),1,".self::MAX_READ_SYMBOLS.") AS text FROM records WHERE id='".$_GET["record_id"]."' LIMIT 1");
 		if ($result[0]["access_mode"]==1 && (string)$_GET["reader"]=="web") msg::error("доступ запрещён!");
 		msg::success($result);
 	}
