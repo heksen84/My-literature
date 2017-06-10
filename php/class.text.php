395a40d019198d920eac008c3cacdf5902b8cd69
Илья <>
2017-06-10 23:21:29 +0600

    ok

--- php/class.text.php
+++ php/class.text.php
@@ -16,6 +16,7 @@ class Text
 	{
 		$db = DataBase::getDB();
 		$result = $db->select("SELECT * FROM records WHERE id='".$_GET["record_id"]."' LIMIT 1");
+//		$result = $db->select("SELECT id, SUBSTR(CAST(text as CHAR),1,30000) AS text FROM records WHERE id='".$_GET["record_id"]."' LIMIT 1");
 		if ($result[0]["access_mode"]==1 && (string)$_GET["reader"]=="web") msg::error("доступ запрещён!");
 		msg::success($result);
 	}
